041214702afdcd1d07cb9ad9a1672384d392461d
mtsuszycki <mt_suszycki@yahoo.com>
2023-06-04 15:13:21 +0700

    Update kube-explain-reformat.sh
    added description to a tree root, cleanups

--- kube-explain-reformat.sh
+++ kube-explain-reformat.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 subtree=$1
-prevs=0
 declare -a position
 index=0
 
@@ -26,45 +25,43 @@ function explain()
 	local arr=("$@")
 	local t=${subtree}.${arr[@]}
 	t=${t// /.}
-	#echo "calling explaing on ${t}"
+	dbg "calling explaing on ${t}"
 	kubectl explain "$t" | sed -n '/^DESCRIPTION:$/,/^FIELDS:$/{/^FIELDS:$/!p;}' | tr '\n' ' ' | sed 's/DESCRIPTION://'
  
 }
 
 fields=0
-echo "$subtree"
+
+description=`explain ""`
+echo "$subtree $description"
+
 while read -r	; do
 	[ -z "$REPLY" ] && continue
+
+	# skip all lines until "FIELDS:"
 	if [ $fields -eq 0 ] ; then
 		[ x"$REPLY" == x'FIELDS:' ] && fields=1 
 		continue
 	fi
+	# kubectl explain uses 2 spaces as indent, convert them to 1 tabulator
 	line=`echo "$REPLY" | sed 's/  /\t/g'`
 	leadspaces=`echo "$line" | awk -F '[^\t].*' '{print length($1)}'`
 	keyword=`echo "$REPLY" | awk '{print $1}'`
 	index=$((leadspaces))
 
-	#if [ $leadspaces -gt $prevs ] ; then
-	#	((index++))
-	#	#echo "Index is $index"
-	#	
-	#elif [ $leadspaces -lt $prevs ] ; then
-	#
-	#	((index += $leadspaces/2))
-	#	#((index--))
-	#fi
 	position[$index]=$keyword
+
+	# pass whole nesting to the explain so it can call kubectl explain pod.metadata.foo.bar etc.
 	description=`explain "${position[@]:0:$((index))}"`
 	tmp=${position[@]:0:$((index))}
 	dbg "---- calling explain on ${tmp// /.}"
 
-	prevs=$leadspaces
-
 	dbg "INDEX: $index, LEADSPACE $leadspaces -$line-, keyword is $keyword, tab is ${position[@]}"
 
+	# print correct number of tabulators, depending on the position in the tree
 	repeat  $((index)) "\t" 
 
-	echo "${position[$index]} ${description:0:500}"
+	echo "${position[$index]} ${description}"
 done
 
-
+ 
